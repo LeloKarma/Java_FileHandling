5105f607de7eaf25eaf92722d3934bfbe19a8cf6
LeloKarma <92229998+LeloKarma@users.noreply.github.com>
2023-11-24 14:49:28 +0100

    BinaryFormatException.java
    Part 1 of Exercise 12.9

---
+++ BinaryFormatException.java
@@ -0,0 +1,8 @@
+import NumberFormatException; 
+
+public class BinaryFormatException extends NumberFormatException {
+    public BinaryFormatException(String message) {
+        super(message);
+    }
+}
+
